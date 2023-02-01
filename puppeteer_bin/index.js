c3ce65d2f15a214666a403e17a0dee6a5e5f6c93
huzhibin@guigeinvest.com <guigeinvest.com>
2023-02-01 15:43:30 +0800

    #0131 查询服务和爬虫服务

--- puppeteer_bin/index.js
+++ puppeteer_bin/index.js
@@ -1,21 +1,4 @@
-const initPuppeteerPool = require('../config/puppeteer_pool');
 const login = require('../server/login')
-const pool = initPuppeteerPool({ // 全局只应该被初始化一次
-    puppeteerArgs: {
-        ignoreHTTPSErrors: true,
-        headless: true,
-        args: [
-            '-–disable-dev-shm-usage',
-            '-–disable-setuid-sandbox',
-            '-–no-first-run',
-            '--no-sandbox',
-            '-–no-zygote',
-            '-–single-process',
-            // `--proxy-server=${proxyUrl}`
-        ],
-        //pipe: true, // 不使用 websocket
-    }
-});
-module.exports = async () => {
+module.exports = async (pool) => {
     return await login(pool);
 }
