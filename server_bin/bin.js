d4d8cf65424fb8121d643cbdd3e54d1c061d6a42
huzhibin@guigeinvest.com <guigeinvest.com>
2023-02-01 15:33:36 +0800

    #0131 查询服务和爬虫服务

--- server_bin/bin.js
+++ server_bin/bin.js
@@ -1,29 +1,30 @@
 const puppeteerBin = require("../puppeteer_bin/index");
 const koaServer = require("./index")
 const Async = require("async");
-let repeatNum = 20;
+let repeatNum = 5;
 const retry = (retries, fn) => {
     fn().then(res => true).catch((err) => retries > 1 ? retry(retries - 1, fn) : false);
 };
 Async.auto({
     koa: function (callback) {
         koaServer()
+        console.log('接口服务启动成功！！');
         callback(null, true);
     },
     puppeteer: function (callback) {
+        console.log('开始启动爬虫服务...');
         puppeteerBin().then(res => {
+            console.log('爬虫服务启动成功！！');
             callback(null, res);
         }).catch(err => {
+            console.log('爬虫服务启动失败！！');
             callback(null, err);
         })
-    },
-    time: function () {
-
     },
     repeat: ['puppeteer', function (results, callback) {
         if (!results.puppeteer) {
             console.log(`${results.puppeteer == 0 ? '登录' : '识别'}失败`);
-            console.log('开始重试！！');
+            console.log('开始重试启动爬虫服务...');
             let type = retry(repeatNum, puppeteerBin);
             callback(null, type);
         } else {
